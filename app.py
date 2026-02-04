a645414f952dc7911eef30336ec89d71729f516d
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-04 20:44:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,10 +51,13 @@ header {visibility: hidden;}
     backdrop-filter: blur(10px);
     border-radius: 20px;
     padding: 25px;
-    margin: -50px auto 30px auto;
+    margin: -10px auto 30px auto;
     max-width: 800px;
     box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
     border: 1px solid rgba(255, 255, 255, 0.5);
+}
+.st-emotion-cache-tn0cau {
+    gap: 1rem !important;  /* Changez cette valeur selon vos besoins */
 }
 .profile-content {
     display: flex;
