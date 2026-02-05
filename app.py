db42d9d2c54f31a079e1ce6a2c15e309e92952ca
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-05 21:23:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -529,7 +529,7 @@ if st.session_state.page == "accueil":
     <div style="display: flex; justify-content: center;">
         <div class="gradient-card" style="padding: 40px 30px;">
             <div style="text-align: center;">
-                <p style="color: #666666; font-size: 16px;">As my Behance portfolio is currently being updated, I have gathered here a selected overview of my work. Below, you will find a PDF featuring my earlier projects, along with a folder showcasing my most recent work.</p>
+                <p style="color: #202124; font-size: 16px;">As my Behance portfolio is currently being updated, I have gathered here a selected overview of my work. Below, you will find a PDF featuring my earlier projects, along with a folder showcasing my most recent work.</p>
             </div>
     """, unsafe_allow_html=True)
 
@@ -567,8 +567,8 @@ if st.session_state.page == "accueil":
         st.markdown("""
             <div class="folder-card">
                 <div style="display: flex; align-items: center; gap: 15px;">
-                    <div style="background: white; padding: 10px; border-radius: 12px; display: flex; align-items: center; justify-content: center; position: absolute; right: 19px; border: 2px solid #d9f0ff;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#d9f0ff" stroke-width="2">
+                    <div style="background: white; padding: 10px; border-radius: 12px; display: flex; align-items: center; justify-content: center; position: absolute; right: 19px; border: 2px solid #9fd8fd;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#9fd8fd" stroke-width="2">
                             <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
                             <polyline points="14,2 14,8 20,8"/>
                             <line x1="16" y1="13" x2="8" y2="13"/>
