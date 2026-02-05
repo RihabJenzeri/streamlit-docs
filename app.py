057b05be410cbc3e6f1dac726ae3a2565f0089e0
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-05 21:15:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -567,8 +567,8 @@ if st.session_state.page == "accueil":
         st.markdown("""
             <div class="folder-card">
                 <div style="display: flex; align-items: center; gap: 15px;">
-                    <div style="background: white; padding: 15px; border-radius: 12px; display: flex; align-items: center; justify-content: center; position: absolute; right: 19px; border: 2px solid ##d9f0ff;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="##d9f0ff" stroke-width="2">
+                    <div style="background: white; padding: 15px; border-radius: 12px; display: flex; align-items: center; justify-content: center; position: absolute; right: 19px; border: 2px solid #d9f0ff;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#d9f0ff" stroke-width="2">
                             <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
                             <polyline points="14,2 14,8 20,8"/>
                             <line x1="16" y1="13" x2="8" y2="13"/>
