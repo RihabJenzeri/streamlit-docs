6cb4d7d96eb390d4ec954b2a3b61ff0a8d45564c
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-05 10:43:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -461,6 +461,18 @@ if st.session_state.page == "accueil":
     st.markdown("<div style='height: 20px;'></div>", unsafe_allow_html=True)
     
     # NOUVELLE CARTE MES DOSSIERS avec boutons intégrés
+    st.markdown("""
+    <div style="display:flex; justify-content:center;">
+      <div class="gradient-card" style="padding:40px 30px; text-align:center;">
+        <p style="color:#666; font-size:16px; max-width:700px; margin:auto;">
+          As my Behance portfolio is currently being updated, I have gathered here
+          a selected overview of my work.
+          Below, you will find a PDF featuring my earlier projects,
+          along with a folder showcasing my most recent work.
+        </p>
+      </div>
+    </div>
+    """, unsafe_allow_html=True)
 
     # Utiliser des colonnes Streamlit pour créer les cartes
     col1, col2 = st.columns(2)
