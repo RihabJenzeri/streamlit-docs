dc7cdd85e63dd7890e29a5ad333a2138f235338b
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-05 16:29:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -622,6 +622,7 @@ if st.session_state.page == "accueil":
 
     # Espace entre les sections
     st.markdown("<div style='height: 20px;'></div>", unsafe_allow_html=True)
+    
     elif st.session_state.page == "medicofi":
     if st.button("← RETOUR"):
         st.session_state.page = "accueil"
