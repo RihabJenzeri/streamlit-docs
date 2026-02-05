d0503226a521c3bc17c9f95b4414d0e6664146df
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-05 20:48:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -931,7 +931,7 @@ elif st.session_state.page == "design_folders":
             <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/>
             <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/>
         </svg>
-        <a href="https://apnidoc.fr/" target="_blank" style="color: #202124; text-decoration: none; font-weight: 500; font-size: 16px;">Visiter https://apnidoc.fr/</a>
+        <a href="https://apnidoc.fr/" target="_blank" style="color: #0563C1; text-decoration: none; font-weight: 500; font-size: 16px;">Visiter https://apnidoc.fr/</a>
     </div>
     """, unsafe_allow_html=True)
     
