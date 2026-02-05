abe7e3abaa8930adc7c2b3b6ec19ee87c5dccc9d
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-05 15:01:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -251,6 +251,8 @@ div[data-testid="column"]:nth-child(2) .st-emotion-cache-1anq8dj:active {
     .st-emotion-cache-2fgyt4 h1 {
     font-size: 1.75rem;
     font-weight: 700;
+    flex-wrap: wrap;
+    justify-content: center;
     /* padding: 1.25rem 0px 1rem; */
 }
 
