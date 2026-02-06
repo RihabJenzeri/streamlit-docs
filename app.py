0afe5400e01a274a21ce0d1cd3b18c7cf7244ec4
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-06 11:56:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -908,8 +908,6 @@ elif st.session_state.page == "apnidoc":
     """, unsafe_allow_html=True)
  
     st.markdown("---")
-    # Website Link Section
-    st.markdown("---")
     st.markdown("""
     <div style="display: flex; align-items: center; gap: 10px;">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FBBDFA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
