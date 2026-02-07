831088ab4f558527f5f9da75b19e74178bf1725e
RihabJenzeri <89862801+RihabJenzeri@users.noreply.github.com>
2026-02-07 06:51:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2812,7 +2812,7 @@ elif st.session_state.page == "medicofi8":
         st.rerun()
 
     # ========== SECTION 1: Affiche Pour les cabinet Sage-femme ==========
-     st.markdown("""
+ st.markdown("""
     <div style="display: flex; align-items: center; gap: 10px; margin-bottom: 20px;">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FBBDFA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
